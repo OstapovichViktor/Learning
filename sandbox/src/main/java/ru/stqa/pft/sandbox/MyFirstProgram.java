b0ef82e2e6d25c8fa6505df2b801590ec2371c5e
OstapovichViktor <51229779+OstapovichViktor@users.noreply.github.com>
2021-03-12 16:41:40 +0300

    add functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,29 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram{
 
   public static void main (String[] args) {
-    System.out.println("Hello, world!!!");
+    hello("world");
+    hello ("everyone");
+    hello ("Viktor");
+
+    double l = 5;
+    System.out.println("Square for " + l + " equal " + area(l));
+
+    double a = 5, b = 7;
+    System.out.println("Square for " + a + " and " + b + " equal " + area(a,b));
   }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody +"!!!");
+  }
+
+  public static double area (double l){
+    return l * l;
+  }
+
+  public static double area (double a, double b){
+    return a * b;
+  }
+
+
 
 }
